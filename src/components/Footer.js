6a5ac4f4040e471fcf8ba1d2470c138f79fe0113
muntasir-fahim <fa3877957@gmail.com>
2023-01-03 01:14:49 +0600

    cart drafted

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,9 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer bg-sky-700 text-sky-50 text-center py-10">
+      <p>&copy; {new Date().getFullYear()} Tech Alpha, All right reserved</p>
+    </div>
+  );
 };
 
 export default Footer;
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,5 +1,15 @@
 const Cart = () => {
-  return <div>Cart</div>;
+  return (
+    <div className="cart-section container mx-auto py-10">
+      <div className="cart-headline">
+        <h2 className="section-title uppercase text-2xl font-bold space-font text-center mb-10">
+          Your cart
+        </h2>
+      </div>
+      <div className="cart-container"></div>
+      <div className="cart-lower-section"></div>
+    </div>
+  );
 };
 
 export default Cart;
