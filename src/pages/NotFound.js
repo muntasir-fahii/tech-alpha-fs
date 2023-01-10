252149a9f3a5ba7fff88aeb1df32457ae8ad216b
muntasir-fahim <fa3877957@gmail.com>
2023-01-11 00:05:24 +0600

    logo added

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,16 @@
+import { MdRemoveShoppingCart } from "react-icons/md";
+
 const NotFound = () => {
   return (
     <div className="mt-10">
       <h2 className="section-title uppercase text-2xl font-bold space-font text-center mb-10">
         Not found
       </h2>
+      <h2 className="section-title uppercase text-9xl font-bold space-font text-center mb-5">
+        404
+      </h2>
+
+      <MdRemoveShoppingCart className=" mx-auto  opacity-40 block text-9xl items-center justify-center" />
     </div>
   );
 };
